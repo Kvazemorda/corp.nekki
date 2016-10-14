af4607114eeff662ad90fc58b64184f20960492b
Ilya <ilyavanavara@gmail.com>
2016-10-14 10:30:12 +0700

    Create project

---
+++ src/main/java/Entity/TestEntity.java
@@ -0,0 +1,77 @@
+package Entity;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+@Entity
+@Table(name = "test", schema = "public", catalog = "test")
+public class TestEntity {
+    private long id;
+    private String content;
+    private Date dateCreated;
+    private String fileName;
+
+    @Id
+    @Column(name = "id", nullable = false, insertable = true, updatable = true)
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "content", nullable = false, insertable = true, updatable = true, length = 1024)
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "dateCreated", nullable = false, insertable = true, updatable = true)
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    @Basic
+    @Column(name = "fileName", nullable = false, insertable = true, updatable = true, length = 1)
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        TestEntity that = (TestEntity) o;
+
+        if (id != that.id) return false;
+        if (content != null ? !content.equals(that.content) : that.content != null) return false;
+        if (dateCreated != null ? !dateCreated.equals(that.dateCreated) : that.dateCreated != null) return false;
+        if (fileName != null ? !fileName.equals(that.fileName) : that.fileName != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + (content != null ? content.hashCode() : 0);
+        result = 31 * result + (dateCreated != null ? dateCreated.hashCode() : 0);
+        result = 31 * result + (fileName != null ? fileName.hashCode() : 0);
+        return result;
+    }
+}
