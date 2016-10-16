08f56eea89c17ddecf84ced7fa72143aba97116e
Ilya <ilyavanavara@gmail.com>
2016-10-16 09:42:33 +0700

    log work

--- src/main/java/Service/FilesXML.java
+++ src/main/java/Service/FilesXML.java
@@ -1,6 +1,6 @@
 package Service;
 
-import Hibernate.HibernateSessionFactory;
+import org.apache.log4j.Logger;
 
 import java.io.File;
 import java.util.concurrent.ExecutorService;
@@ -12,6 +12,7 @@ import java.util.concurrent.Executors;
 public class FilesXML {
     private String path;
     private File[] files;
+    private static Logger logger = Logger.getLogger(FilesXML.class);
 
     public FilesXML(String path) {
         this.path = path;
@@ -19,8 +20,8 @@ public class FilesXML {
         //directory is directory or it exists?
         if(catalogFiles.exists() || catalogFiles.isDirectory()){
             files = catalogFiles.listFiles();
-            if(files == null){
-                System.out.println("Directory don't have files");
+            if(files.length == 0){
+                logger.info("В директории нет файлов для чтения");
             }else {
                 getFilesToDirectory();
             }
@@ -35,7 +36,6 @@ public class FilesXML {
         ExecutorService service = Executors.newFixedThreadPool(10);
 
         for(final File file: files){
-
             // file is file and is XML file
             if(file.isFile() && isXMLFile(file)) {
                 service.execute(new Runnable() {
@@ -45,25 +45,13 @@ public class FilesXML {
                         parser.startParsXML(file);
                     }
                 });
-            }
-        }
-
-        service.shutdown();
-
-        while (true){
-            if(!service.isShutdown()){
-                HibernateSessionFactory.shutdown();
             }else{
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                File fileWrong = new File(ReaderProperties.dirWrongFiles + file.getName());
+                file.renameTo(fileWrong);
+                logger.info("В папке для чтения нет XML файлов");
             }
-
         }
-
-
+        service.shutdown();
     }
 
     /**
